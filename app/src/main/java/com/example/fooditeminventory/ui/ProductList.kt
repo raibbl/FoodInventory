4aecc62137779560f649e14bdffdfb0a623eb7eb
raibbl <raibbl1997@gmail.com>
2024-06-23 03:29:44 -0500

    use more material3 ProductList

--- app/src/main/java/com/example/fooditeminventory/ui/ProductList.kt
+++ app/src/main/java/com/example/fooditeminventory/ui/ProductList.kt
@@ -5,17 +5,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
+import androidx.compose.material3.Card
 import androidx.compose.material.DismissDirection
 import androidx.compose.material.DismissState
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.material.SwipeToDismiss
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material.rememberDismissState
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,7 +26,7 @@ import androidx.navigation.NavController
 import com.example.fooditeminventory.db.ProductEntity
 import com.example.fooditeminventory.ui.home.HomeFragmentDirections
 import androidx.compose.material3.MaterialTheme
-@OptIn(ExperimentalMaterialApi::class)
+
 @Composable
 fun ProductList(products: List<ProductEntity>, onDelete: (ProductEntity) -> Unit, navController: NavController) {
     if (products.isEmpty()) {
@@ -124,15 +125,16 @@ fun SwipeToDismissBackground(dismissState: DismissState) {
 @Composable
 fun ProductItem(product: ProductEntity, onClick: () -> Unit) {
     Card(
-        elevation = 4.dp,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp)
+            .padding(vertical = 8.dp)
             .clickable(onClick = onClick)
     ) {
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.onBackground)
+                .fillMaxWidth()
                 .padding(12.dp)
         ) {
             Text(text = product.name, style = MaterialTheme.typography.headlineSmall, color = MaterialTheme.colorScheme.onSurface)

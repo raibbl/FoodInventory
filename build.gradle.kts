273ebe36a33828775ab921cb49f7253c6169439c
raibbl <raibbl1997@gmail.com>
2024-09-15 20:25:58 -0500

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:8.2.2")
+        classpath ("com.android.tools.build:gradle:8.6.0")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
     }
